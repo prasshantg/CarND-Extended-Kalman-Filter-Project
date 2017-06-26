eadd7db67b51f7edd17f43d71a34b96f3b86ba22
Prashant Gaikwad <pgaikwad@nvidia.com>
2017-06-26 11:21:03 +0530

    implement EKF for Lidar and Radar

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -18,22 +18,59 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+   * update the state by using Extended Kalman Filter equations
+   */
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float rho = sqrt((px * px) + (py * py));
+
+  if(fabs(rho) < 0.0001){
+    rho = 0.0001;
+  }
+
+  float theta = atan2(py, px);
+  float ro_dot = ((px * vx) + (py *vy)) / rho; 
+  VectorXd z_pred = VectorXd(3);
+
+  theta = atan2(sin(theta), cos(theta));
+  z_pred << rho, theta, ro_dot;
+
+  VectorXd y_vector = z - z_pred;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;  
+
+  MatrixXd I;
+  I = MatrixXd::Identity(x_.size(), x_.size());
+  //new state
+  x_ = x_ + (K * y_vector);
+  P_ = (I - K * H_) * P_;  
 }
